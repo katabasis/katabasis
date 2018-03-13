0b8eabbd5bea82addec8e2007c25b0c466894925
trabing <kenso.trabing@bloomwebsite.com>
2018-03-12 20:53:34 -0400

    Rename application as katabasis

--- run/wsgi.py
+++ run/wsgi.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from application import omnibus
+from katabasis import omnibus
 
-# TODO Replace the invocation an industrial strength application server
+# TODO Replace the invocation an industrial strength katabasis server
 #
 # The following parameters are specified for the explicit purpose of
-# expediting the development process of this application. They are not
+# expediting the development process of this katabasis. They are not
 # suitable for production and should be changed before deployment.
 #
 omnibus.run(host='127.0.0.1', port=5000, debug=True)
